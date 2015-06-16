90b0f5f49745f6e9af1d007a644fa21f70f314c8
santamaria <santamaria@SANTAMARILAPTOP>
2015-06-16 16:01:12 -0430

    bueno ya imprimio la primera parte con to string pero se acabo el tiempo programo muy rapido y no dio chance de seguirle el ritmo pero explico como funciona su codigo el cual debemos descargarlo de github y probar hacerlo nosotros

---
+++ JavaDiaDos/src/javadiados/ClaseSeccion.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javadiados;
+
+/**
+ *
+ * @author santamaria
+ */
+public class ClaseSeccion {
+    
+    private int numeroSeccion;
+    private ClaseProfesor profesorAsigado;
+    private List<ClaseEstudiante>estudiates;
+    
+    
+    
+}
